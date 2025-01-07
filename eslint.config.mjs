487f85998a25965e878dc16aeb9793e4f4a6b405
Arijit Banerjee <ari.working.banerjee@gmail.com>
2025-01-07 12:00:06 +0530

    change again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,38 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    eslintConfigPrettier,
-    {
-        rules: {
-            // Possible errors
-            'no-console': 'warn',
-            'no-debugger': 'error',
-            'no-dupe-keys': 'error',
-            'no-duplicate-case': 'error',
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      // Possible errors
+      "no-console": "warn",
+      "no-debugger": "error",
+      "no-dupe-keys": "error",
+      "no-duplicate-case": "error",
 
-            // Best practices
-            eqeqeq: 'error',
-            'no-eval': 'error',
-            'no-implicit-coercion': 'error',
+      // Best practices
+      eqeqeq: "error",
+      "no-eval": "error",
+      "no-implicit-coercion": "error",
 
-            // Variables
-            'no-unused-vars': 'warn',
-            'no-undef': 'error',
-            'no-shadow': 'error',
-            'no-redeclare': 'error',
+      // Variables
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "no-shadow": "error",
+      "no-redeclare": "error",
 
-            // Code complexity
-            'max-lines': ['warn', 300],
-            'max-nested-callbacks': ['warn', 3],
+      // Code complexity
+      "max-lines": ["warn", 300],
+      "max-nested-callbacks": ["warn", 3],
 
-            // ES6
-            'prefer-const': 'error',
-            'no-duplicate-imports': 'error',
-        },
+      // ES6
+      "prefer-const": "error",
+      "no-duplicate-imports": "error",
     },
+  },
 ];
