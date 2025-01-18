e36d66b6601c959ca8e95eb54ff654be68ce309d
Arijit Banerjee <ari.working.banerjee@gmail.com>
2025-01-18 12:48:49 +0530

    glallery section is completed with icon issues.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ const btnPerv = document.querySelector(".slider__btn--left");
 const btnNext = document.querySelector(".slider__btn--right");
 const dotsContainer = document.querySelector(".dots");
 const imageInfo = document.querySelector(".image-info");
+const imageWrapper = document.querySelector(".image-wrapper");
+const btnLeft = document.querySelector(".gallery__btn--left");
+const btnRight = document.querySelector(".gallery__btn--right");
 
 // Global variables
 let curSlide = 0; // the current slide with translateX(0%)
@@ -53,7 +56,7 @@ const prevSlide = function () {
   createInfo(curSlide);
 };
 
-const createInfo = function (slide) {  
+const createInfo = function (slide) {
   // Put that into the desc data attribute of imageInfo
   imageInfo.dataset.desc = slides[slide].title;
 };
@@ -77,6 +80,18 @@ dotsContainer.addEventListener("click", function (e) {
 document.addEventListener("keydown", e => {
   if (e.key === "ArrowLeft") prevSlide();
   if (e.key === "ArrowRight") nextSlide();
+});
+
+btnRight.addEventListener("click", function () {
+  imageWrapper.style.transform = "translateX(-59.5%)";
+  btnLeft.style.transform = "translate(0%, -50%)";
+  btnRight.style.transform = "translate(100%, -50%)";
+});
+
+btnLeft.addEventListener("click", function () {
+  imageWrapper.style.transform = "translateX(0%)";
+  btnRight.style.transform = "translate(0%, -50%)";
+  btnLeft.style.transform = "translate(-100%, -50%)";
 });
 
 // A function to set the initial things up
