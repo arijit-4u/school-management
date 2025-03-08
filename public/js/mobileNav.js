c4a428b31be43d0ffef1b666a9fae8386c061ac2
KSayanta <sayantakarmakar1999@gmail.com>
2025-03-09 00:51:56 +0530

    feat: add nav sub menu

--- public/js/mobileNav.js
+++ public/js/mobileNav.js
@@ -2,6 +2,7 @@
 const nav = document.querySelector(".mobile-nav");
 const btnMenu = document.getElementById("btnCollapsed");
 const btnMenuClose = document.getElementById("btnExpanded");
+const subMenuList = document.querySelectorAll(".mobile-nav__links");
 
 // Handler Function
 const menuOpen = function () {
@@ -12,7 +13,23 @@ const menuClose = function () {
   nav.classList.remove("open");
 };
 
+const subMenuToggle = function (subMenu) {
+  const isMenuOpen = subMenu.getAttribute("aria-expanded");
+
+  if (isMenuOpen === "false") {
+    subMenu.setAttribute("aria-expanded", true);
+  } else {
+    subMenu.setAttribute("aria-expanded", false);
+  }
+};
+
 // Eventlistners
 btnMenu.addEventListener("click", menuOpen);
 
 btnMenuClose.addEventListener("click", menuClose);
+
+subMenuList.forEach(subMenu => {
+  subMenu.firstElementChild.addEventListener("click", () => {
+    subMenuToggle(subMenu);
+  });
+});
